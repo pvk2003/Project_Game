4895cced5be9a4e7c21e9431f700af420fa612ef
PhamViKhang <21057711.khang@student.iuh.edu.vn>
2024-10-19 22:41:00 +0700

    Project_Game

---
+++ DuAnGameRunner1/Assets/Script/Player_Controller.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Controller : MonoBehaviour
+{
+    [SerializeField] Transform center_pos;
+    [SerializeField] Transform right_pos;
+    [SerializeField] Transform left_pos;
+
+    int current_pos = 0;
+    public float side_speed;
+    public float running_speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+        current_pos = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + running_speed * Time.deltaTime);
+
+        if (current_pos == 0)
+        {
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                current_pos = 1;
+            }
+
+            else if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                current_pos = 2;
+            }
+        }
+        else if (current_pos == 1)
+        {
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                current_pos = 0;
+            }
+        }
+        else if (current_pos == 2)
+        {
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                current_pos = 0;
+            }
+        }
+
+        if (current_pos == 0)
+
+        {
+            if (Vector3.Distance(transform.position, new Vector3(center_pos.position.x, transform.position.y, transform.position.z)) >= 0.1f)
+            {
+                Vector3 dir = new Vector3(center_pos.position.x, transform.position.y, transform.position.z) - transform.position;
+                transform.Translate(dir.normalized * side_speed * Time.deltaTime, Space.World);
+            }
+        }
+        else if (current_pos == 1)
+        {
+            if (Vector3.Distance(transform.position, new Vector3(left_pos.position.x, transform.position.y, transform.position.z)) >= 0.1f)
+            {
+                Vector3 dir = new Vector3(left_pos.position.x, transform.position.y, transform.position.z) - transform.position;
+                transform.Translate(dir.normalized * side_speed * Time.deltaTime, Space.World);
+            }
+        }
+        else if (current_pos == 2)
+        {
+            if (Vector3.Distance(transform.position, new Vector3(right_pos.position.x, transform.position.y, transform.position.z)) >= 0.1f)
+            {
+                Vector3 dir = new Vector3(right_pos.position.x, transform.position.y, transform.position.z) - transform.position;
+                transform.Translate(dir.normalized * side_speed * Time.deltaTime, Space.World);
+            }
+        }
+    }
+}
