f45000d8350341431fe2a43b9cc86e5103864d18
kz16sep <itzkun1609@gmail.com>
2024-10-20 23:12:30 +0700

    add .

--- DuAnGameRunner1/Assets/Script/Player_Controller.cs
+++ DuAnGameRunner1/Assets/Script/Player_Controller.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Callbacks;
 using UnityEngine;
 
 public class Player_Controller : MonoBehaviour
@@ -7,15 +8,14 @@ public class Player_Controller : MonoBehaviour
     [SerializeField] Transform center_pos;
     [SerializeField] Transform right_pos;
     [SerializeField] Transform left_pos;
-
+    [SerializeField] Rigidbody rb;
     int current_pos = 0;
     public float side_speed;
     public float running_speed;
+    public float jump_force;
     // Start is called before the first frame update
     void Start()
     {
-
-
         current_pos = 0;
     }
 
@@ -75,6 +75,11 @@ public class Player_Controller : MonoBehaviour
                 Vector3 dir = new Vector3(right_pos.position.x, transform.position.y, transform.position.z) - transform.position;
                 transform.Translate(dir.normalized * side_speed * Time.deltaTime, Space.World);
             }
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = Vector3.up * jump_force;
         }
     }
 }
