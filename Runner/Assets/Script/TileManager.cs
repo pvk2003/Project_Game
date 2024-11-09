843ec532239b99d086a1eca0e81aa4def9e72041
lenam1409 <hoangnam1492003@gmail.com>
2024-11-09 12:50:48 +0700

    add .

---
+++ Runner/Assets/Script/TileManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour
+{
+    public GameObject[] tilePrefabs;
+    public float zSpawn = 0;
+    public float tileLength = 30;
+    public int numberOfTiles = 5;
+    public Transform player;
+    
+    private List<GameObject> activeTiles = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < numberOfTiles; i++)
+        {
+            SpawnTile(Random.Range(0, tilePrefabs.Length));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.position.z > zSpawn - (numberOfTiles * tileLength))
+        {
+            SpawnTile(Random.Range(0, tilePrefabs.Length));
+            DeleteTile();
+        }
+    }
+
+    public void SpawnTile(int tileIndex)
+    {
+        GameObject tile = Instantiate(tilePrefabs[tileIndex], transform.forward * zSpawn, transform.rotation);
+        activeTiles.Add(tile);
+        zSpawn += tileLength;
+    }
+
+    private void DeleteTile()
+    {
+        // Delete the oldest tile and remove it from the list
+        Destroy(activeTiles[0]);
+        activeTiles.RemoveAt(0);
+    }
+}
